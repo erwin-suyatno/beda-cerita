67d18f811a9ee23beb376bc301dc3ac99f0a5052
Erwin Suyatno <84340934+erwin-suyatno@users.noreply.github.com>
2023-11-08 10:04:43 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default {
 
 .footer-container {
   position: fixed;
-  top: 0;
+  bottom: 0;
   left: 0;
   right: 0;
   z-index: 9999; /* Atur nilai z-index untuk memastikan header muncul di atas konten lainnya */
